2cb5709d1caf3f41df61b7b78227c5382f912b15
Muhammad Saad <saadfenixel@gmail.com>
2026-04-05 22:19:44 +0500

    Start wc/
    Co-authored-by: alt <k250604@nu.edu.pk>

---
+++ src/3_hard/_wc/wc.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+using std::cout;
+
+
+int main(int argc, char* argv[]){
+    if (argc == 1){
+        cout << "[!] Expected 1 argument, got " << (argc - 1) << ". :(\n";
+    }
+    else{
+        std::filesystem::path path = argv[1];
+
+        if (!std::filesystem::exists(path)){
+            cout << "[!] wc: failed to wordcount '" << path.string() << "': file doesn't exist.\n";
+            return 0;
+        }
+        
+        if (!std::filesystem::is_regular_file(path)){
+            cout << "[!] wc: failed to wordcount '" << path.string() << "': Arg is not a file.\n";
+            return 0;
+        }
+
+        int lineCount {0};
+        int wordCount {0};
+        int charCount {0};
+        
+        std::ifstream file(path);
+        if (file.is_open()){
+            std::string line;
+
+            while (std::getline(file, line)){
+                lineCount++;
+
+                for (auto& character : line){
+                    if (isalpha(character)){
+                        charCount++;
+                    }
+                    if (character == ' ' || character == '\t' || character == '\n'){
+                        wordCount++;
+                    }
+                }
+            }
+
+            cout << lineCount << " " << wordCount << " " << charCount << " (" << path.string() << ")\n"; 
+        }
+    }
+}
